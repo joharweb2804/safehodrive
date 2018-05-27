7aa0827d932a97ccc3af7a82300845f35d0fe638
joharweb2804 <39568189+joharweb2804@users.noreply.github.com>
2018-05-27 15:31:19 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -21,12 +21,12 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="05/watching-forex-volatility-on-currensee.html"}
-if(x=="2"){xxx="05/volume-rate-of-change-indicator-and.html"}
-if(x=="3"){xxx="05/understanding-triangle-formation.html"}
-if(x=="4"){xxx="05/usd-fundamental-analysis.html"}
-
-a_to_ve[i].href="https://safehodrive.blogspot.co.id/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="11/dailyforex-blog-reviews-forex-initiate.html"}
+if(x=="2"){xxx="11/forex-social-platforms-dailyforex.html"}
+if(x=="3"){xxx="11/forex-trading-ifx-markets-review.html"}
+if(x=="4"){xxx="11/how-to-build-winning-trading-plan.html"}
+if(x=="5"){xxx="11/is-it-time-to-quit-indicators-and-turn.html"}
+output.value = "https://safehodrive.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
